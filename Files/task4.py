839e99b6fdcd28718f16c737da5c781caf63581d
Turchin_Alexander <ymeleca@gmail.com>
2022-08-17 14:50:04 +0300

    changed task1 and added task4 in files' theme

--- Files/task4.py
+++ Files/task4.py
@@ -1,3 +1,10 @@
 # 4) С клавиатуры в одной строке вводится произвольное количество вещественных чисел.
 # Запишите их в файл, расположив каждое число на отдельной строке.
 
+float_numbers = input('Input the search string:').split()
+
+# float_numbers = ('1 2 3 4 5 6 7 8 8 8888').split()  # for example
+
+with open('float.txt', 'w', encoding='utf-8') as f:
+    for i in float_numbers:
+        f.write(i + '\n')
