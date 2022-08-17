72b4ac81a7d14dc40a26bfc59dc6c51acf1f0b33
Turchin_Alexander <ymeleca@gmail.com>
2022-08-17 10:29:35 +0300

    updeted functijns_tasks

--- Functions/task2.py
+++ Functions/task2.py
@@ -1,14 +1,14 @@
 # Имеется список с произвольными данными. Необходимо преобразовать его в множество.
 # Если какие-то элементы нельзя хешировать, то пропускаем их.  Результаты выведите на экран.
 
-_list = [1, [2], 46, 46, {1}, {2: 1}, (1, 2), 'qwerty', 3.45]
+def list_to_set(list_in):
+    _set = set()
+    for i in list_in:
+        try:
+            _set.add(i)
+        except Exception:
+            pass
+    return _set
 
-# _set = {}
-_set = set()
-for i in _list:
-    try:
-        _set.add(i)
-    except (AttributeError, TypeError):
-        pass
-print(_set)
-print(type(_set))
+result = list_to_set([1, [2], 46, 46, {1}, {2: 1}, (1, 2), 'qwerty', 3.45])  # for example
+print(result)
