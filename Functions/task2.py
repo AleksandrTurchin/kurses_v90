ce85f01e2d954e7070610ba354d1b44b68861263
Turchin_Alexander <ymeleca@gmail.com>
2022-08-17 10:35:10 +0300

    updeted functions_tasks

--- Functions/task2.py
+++ Functions/task2.py
@@ -2,13 +2,14 @@
 # Если какие-то элементы нельзя хешировать, то пропускаем их.  Результаты выведите на экран.
 
 def list_to_set(list_in):
-    _set = set()
+    _set = set()            # создаем переменную множества, куда будем добавлять данные из списка list_in
     for i in list_in:
-        try:
+        try:                # с помощью set.add(item) добавляем данные из списка list_in, а try-except отлавливаем TypeError
             _set.add(i)
-        except Exception:
+        except TypeError:
             pass
     return _set
 
 result = list_to_set([1, [2], 46, 46, {1}, {2: 1}, (1, 2), 'qwerty', 3.45])  # for example
 print(result)
+# {1, 3.45, (1, 2), 'qwerty', 46}
