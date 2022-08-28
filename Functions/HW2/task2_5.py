b0cd24744bf1e98e68ec0da5c1b3360c085eb2d4
AleksandrTurchin <turchin.alexander.n@gmail.com>
2022-08-28 09:44:18 +0300

    updeted homeworks

--- Functions/HW2/task2_5.py
+++ Functions/HW2/task2_5.py
@@ -1,17 +1,22 @@
 # Написать функцию-декоратор, которая вычисляет время выполнения декорируемой функции,
 # а также выводит на экран имя функции и ее параметры.
 
-import inspect
+from inspect import signature
+# import inspect
+
+"""import time
+start_time = time.time()
+main()
+print("--- %s seconds ---" % (time.time() - start_time))"""   # https://askdev.ru/q/kak-poluchit-vremya-vypolneniya-programmy-python-1330/
+
 from datetime import datetime
-
-
 def decorating_func(own_func):
     def wrapper(*args, **kwargs):
         time_now = datetime.now()
         result = own_func(*args, **kwargs)
         print(f'Время выполнения декорируемой функции: {datetime.now() - time_now}')
-        print(f'Имя функции: {own_func.__qualname__}')
-        print(f'Параметры функции: {inspect.signature(own_func)}')
+        print(f'Имя функции: {own_func.__name__}')                                      # __name__ or __qualname__
+        print(f'Параметры функции: {signature(own_func)}')                              #https://docs.python.org/3/library/inspect.html#introspecting-callables-with-the-signature-object
         return result
     return wrapper
 
@@ -23,5 +28,5 @@ def sum_range(start, end):
     return sum(range(start, end + 1))
 
 
-print(sum_range(1, 1000900))
+print(sum_range(1, 10009000))
 
