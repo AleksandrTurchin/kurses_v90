269a6aa4c88d56290baa309bc02e0b630bf86ac8
AleksandrTurchin <turchin.alexander.n@gmail.com>
2022-10-06 18:46:43 +0300

    added homeworks about Stand_modules

---
+++ Stand_modules/task5.py
@@ -0,0 +1,6 @@
+"""
+Напишите функцию для создания строки HTML с тегами вокруг слов (использовать шаблону из модуля string).
+Пример функции и результата:
+add_tags('i', 'Python') -> '<i>Python</i>'
+add_tags('b', 'Python Tutorial') -> '<b>Python Tutorial </b>'
+"""
