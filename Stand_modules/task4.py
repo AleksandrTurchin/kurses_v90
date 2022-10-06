fa5eac67f7f36846705639d777168aa177461552
AleksandrTurchin <turchin.alexander.n@gmail.com>
2022-10-06 21:33:51 +0300

    added homeworks about Stand_modules

--- Stand_modules/task4.py
+++ Stand_modules/task4.py
@@ -1,3 +1,12 @@
 """
 Напишите программу Python для отображения календаря для в заданной локали. (модуль calendar)
+https://docs-python.ru/standart-library/modul-calendar-python/tekstovyj-html-kalendari-russkom-jazyke/
+https://docs-python.ru/standart-library/modul-calendar-python/funktsii-atributy-modulja-calendar/
+https://tonais.ru/osnovy/modul-calendar-v-python
 """
+
+import calendar
+c = calendar.LocaleTextCalendar(locale='en_US.UTF-8')
+print(c.prmonth(2022, 10))
+
+# print(c.formatmonth(2022, 10))
--- Stand_modules/task5.py
+++ Stand_modules/task5.py
@@ -3,4 +3,17 @@
 Пример функции и результата:
 add_tags('i', 'Python') -> '<i>Python</i>'
 add_tags('b', 'Python Tutorial') -> '<b>Python Tutorial </b>'
+
+https://www.geeksforgeeks.org/template-class-in-python/
 """
+
+from string import Template
+
+def add_tags(tag, word):
+	v = {'tag': tag, 'word': word}
+	s = Template('<$tag>$word</$tag>')
+	return s.substitute(v)                              # f'<{tag}>{word}</{tag}>'
+
+# tests
+print(add_tags('i', 'Python'))
+print(add_tags('b', 'Python Tutorial'))
