8c8dd0bcf744120ae99ac31b6a6b9e611b6f67a8
AleksandrTurchin <turchin.alexander.n@gmail.com>
2022-10-25 19:32:50 +0300

    added homeworks about Standart Modules

--- Stand_modules/task3.py
+++ Stand_modules/task3.py
@@ -1,7 +1,44 @@
 """
-Напишите игру в кости. ????????????
+Напишите игру в кости.
+
+https://docs-python.ru/standart-library/modul-random-python/sluchajnye-tselye-chisla-modulja-random/
+https://docs-python.ru/standart-library/modul-sys-python/funktsija-exit-modulja-sys/
 """
 
+import sys
 from random import randint
-print(randint(1, 6))
 
+print('Добро пожаловать в игру "Кости". Правила просты: бросаем пару костей, у кого результат больше тот и выиграл')
+answer = input('Играем? (Напиши ответ: Да или Нет): ')
+
+if answer.lower() == 'да':
+    print('Тогда начнем игру!')
+else:
+    print('Пока! Заходи еще!')
+    sys.exit()
+
+player_one = input('Введите имя игрока 1:  ')
+player_two = input('Введите имя игрока 2:  ')
+
+input('{} нажмите клавишу enter, чтобы бросить кости'.format(player_one))
+
+dice1_player_one = randint(1, 6)
+dice2_player_one = randint(1, 6)
+sum_dices_player_one = dice1_player_one + dice2_player_one
+
+print('У игрока {} значение 1-ого кубика {}, значение 2-ого кубика {}, сумма двух кубиков {}'.format(player_one, dice1_player_one, dice2_player_one, sum_dices_player_one))
+
+input('{} нажмите клавишу enter, чтобы бросить кости'.format(player_two))
+
+dice1_player_two = randint(1, 6)
+dice2_player_two = randint(1, 6)
+sum_dices_player_two = dice1_player_two + dice2_player_two
+
+print('У игрока {} значение 1-ого кубика {}, значение 2-ого кубика {}, сумма двух кубиков {}'.format(player_two, dice1_player_two, dice2_player_two, sum_dices_player_two))
+
+if sum_dices_player_one > sum_dices_player_two:
+    print('Игрок {} выиграл! Поздравляем!!'.format(player_one))
+elif sum_dices_player_one < sum_dices_player_two:
+    print('Игрок {} выиграл! Поздравляем!!'.format(player_two))
+else:
+    print('Ничья! Победила дружба!! :)')
--- Stand_modules/task6.py
+++ Stand_modules/task6.py
@@ -1,5 +1,5 @@
 """
-??????????
+
 Напишите скрипт для отображения различных форматов даты и времени.
 а) Текущая дата и время
 б) Текущий год
@@ -9,15 +9,20 @@
 е) День года
 г) день месяца
 з) День недели
+
+https://pythonpip.ru/osnovy/funktsiya-strftime-v-python-formaty-vremeni-i-daty
+https://docs-python.ru/standart-library/modul-datetime-python/kody-formatirovanija-strftime-strptime-modulja-datetime/
+https://docs-python.ru/standart-library/modul-datetime-python/klass-datetime-modulja-datetime/
+https://docs-python.ru/standart-library/modul-datetime-python/primery-ispolzovanija-datetime-date/
+https://docs-python.ru/standart-library/modul-datetime-python/klass-date-modulja-datetime/
 """
 
-import time
 import datetime
-print("Текущая дата и время: ", datetime.datetime.now())
-print("Текущий год: ", datetime.date.today().strftime("%Y"))
-print("Месяц года: ", datetime.date.today().strftime("%B"))
-print("номер недели в году: ", datetime.date.today().strftime("%W"))
-print("будний день недели: ", datetime.date.today().strftime("%w"))
-print("День года: ", datetime.date.today().strftime("%j"))
-print("день месяца : ", datetime.date.today().strftime("%d"))
-print("День недели: ", datetime.date.today().strftime("%A"))
+print('Текущая дата и время: ', datetime.datetime.now())
+print('Текущий год: ', datetime.date.today().strftime('%Y'))
+print('Месяц года: ', datetime.date.today().strftime('%B'))
+print('номер недели в году: ', datetime.date.today().strftime('%W'))
+print('будний день недели: ', datetime.date.today().strftime('%w'))
+print('День года: ', datetime.date.today().strftime('%j'))
+print('день месяца: ', datetime.date.today().strftime('%d'))
+print('День недели: ', datetime.date.today().strftime('%A'))
